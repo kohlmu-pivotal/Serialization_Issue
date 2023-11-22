85c43097fd5ec39697d0dfd6dfc8078b1b3153f4
Udo Kohlmeyer <udo@vmare.com>
2023-11-23 09:03:58 +1100

    updating Order object to reflect the one provided

--- src/main/java/com/vmware/gemfire/jpmc/Order.java
+++ src/main/java/com/vmware/gemfire/jpmc/Order.java
@@ -3,12 +3,13 @@ package com.vmware.gemfire.jpmc;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.gemfire.mapping.annotation.Region;
 
+import java.io.Serializable;
+
 @lombok.Data
 @lombok.AllArgsConstructor
 @lombok.Builder
-@lombok.Setter
 @Region("Orders")
-public class Order {
+public class Order implements Serializable {
   @Id
   private String orderId;
   private String settlementInstruction;
