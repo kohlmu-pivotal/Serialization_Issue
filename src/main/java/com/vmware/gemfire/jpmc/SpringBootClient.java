5d0b2db3e48ef6e7248e235a69111ab414887282
Udo Kohlmeyer <udo@vmare.com>
2023-11-23 08:28:29 +1100

    Added Web Endpoint

--- src/main/java/com/vmware/gemfire/jpmc/SpringBootClient.java
+++ src/main/java/com/vmware/gemfire/jpmc/SpringBootClient.java
@@ -1,10 +1,7 @@
 package com.vmware.gemfire.jpmc;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.gemfire.GemfireTemplate;
 
 @SpringBootApplication
 public class SpringBootClient {
